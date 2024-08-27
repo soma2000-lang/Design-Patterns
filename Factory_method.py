e61ab6337099e9d192676faefbf8bd04876f9385
Somasree <bishnupadamajumder32@gmail.com>
2024-08-27 16:12:19 +0530

    Update at Tue, 27 Aug 2024 10:42:19 GMT

---
+++ Factory_method.py
@@ -0,0 +1,6 @@
+#
+
+    #a class cant anticipate the class of objects it must create
+    #a class wants its subclasses to specify the objects it creates
+    #classes delegate responsibility to one of several helper subclasses, and you want to localize the knowledge of which helper subclass is the delegate
+
