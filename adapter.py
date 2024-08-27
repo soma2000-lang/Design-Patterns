8ea5589144c9674625f5bc115a498af89927ca5a
Somasree <bishnupadamajumder32@gmail.com>
2024-08-27 15:36:04 +0530

    Update at Tue, 27 Aug 2024 10:06:04 GMT

--- adapter.py
+++ adapter.py
@@ -1,1 +1,19 @@
-# you want to create resuable class
+# you want to create resuable class that cooperates  with classes thta dont necessariy have compatible interfaces
+import sys
+class Target:
+    def request(self):
+        pass
+class Adaptee: # this is the one which will give request
+     def  specificRequest(self):
+         print("Specific requeust")
+class Adapter(Target,Adaptee):
+    def __init__(self) -> None:
+        Adaptee__init__(self)
+        Target__init__(self)
+    def request(self):
+        return self.specificRequest()
+if __name__ == '__main__':
+    t= Adapter()
+    t.request()
+    
+    
