ab3f980aabd394cbd419365d5dac90d0a9a2b91a
Somasree <bishnupadamajumder32@gmail.com>
2024-08-27 16:43:35 +0530

    Update at Tue, 27 Aug 2024 11:13:35 GMT

---
+++ TemplateMethod.py
@@ -0,0 +1,4 @@
+# Template method defines the skeleton of an algorithm in an operation, deferring some
+# steps to subclasses. It lets subclasses redefine certain steps of an algorithm 
+# without changing the algorithm's structure. The pattern has behavioral purpose and
+# applies to the classes.
