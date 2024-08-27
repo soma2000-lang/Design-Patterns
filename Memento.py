5223680b0bc0d1062d532a6fa0cd583f3984eb47
Somasree <bishnupadamajumder32@gmail.com>
2024-08-27 15:59:45 +0530

    Update at Tue, 27 Aug 2024 10:29:45 GMT

---
+++ Memento.py
@@ -0,0 +1,1 @@
+#Memento without violating encapsulation, captures and externalizes an object's internal state so that the object can be restored to this state later. The pattern has behavioral purpose and applies to the objects.
