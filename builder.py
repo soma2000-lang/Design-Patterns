91399bd19e54c1f782abd2ae54e6e88937cad8f7
Somasree <bishnupadamajumder32@gmail.com>
2024-08-27 15:36:04 +0530

    Update at Tue, 27 Aug 2024 10:06:04 GMT

--- builder.py
+++ builder.py
@@ -1,1 +1,17 @@
-#ledthe algorithm for craeting an object #hould be
+the algorithm  for creating an object should be independant of the parts
+and how it should be assembled,often used  building constrcuting objects
+
+import sys
+
+class Product:
+    def __init__(self):
+        self._partA=""
+        self._partB=""
+        self._partC=""
+        def  makeA(self,part):
+            self._partA= part
+        def makeB(self,part):
+            self._partB=part
+        def makeC(self,part):
+            self._partC=part
+        
