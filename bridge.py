6733721d03f5e48dc740eeb4926e3fab4b04c97f
Somasree <bishnupadamajumder32@gmail.com>
2024-08-27 15:36:04 +0530

    Update at Tue, 27 Aug 2024 10:06:04 GMT

--- bridge.py
+++ bridge.py
@@ -1,1 +1,26 @@
-# decoy
+# decouple an abstarction from its implementation so that they can vary independantly
+#use it when you want to avoid a permanent binding between an abstarction and its impelmentation
+
+import  sys
+    class Implementator:
+        def action(self):
+            pass
+    class ConcreteImplementorA(Implementator):
+        def action(self):
+            print("Concrete Implematator A")
+    class ConcreteImplementatorB(Implementator):
+        def action(self):
+            print("Concrete Implemnattor B")
+    class Bridge:
+        def__init_(self,implementation):
+            self._implementator=implementation
+        def operation(self):
+            self._implementator.action()
+    if __name__=="__main__":
+        bridge= Bridge(ConcreteImplementorA())
+        bridge.operation()
+        
+        
+        bridge=Bridge(ConcreteImplementatorB)
+        bridge.operation()
+            
